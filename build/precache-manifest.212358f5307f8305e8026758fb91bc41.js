89d3f79ae2cc8ff2bc9ebc7976d3e05e98a75c20
NelliPestych <NelliPestych@bigmir.net>
2020-06-27 08:32:44 +0300

    another commit

---
+++ build/precache-manifest.212358f5307f8305e8026758fb91bc41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4a6b8769af17d169dedcbacc5eb0c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed7d8bec657dc31d0a67",
+    "url": "/static/css/main.b4197361.chunk.css"
+  },
+  {
+    "revision": "578ba553ce8ebccbbcfe",
+    "url": "/static/js/2.2c1fb9df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c1fb9df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7d8bec657dc31d0a67",
+    "url": "/static/js/main.53fb05b7.chunk.js"
+  },
+  {
+    "revision": "ed45e12a076b2da98fe4",
+    "url": "/static/js/runtime-main.bdce7253.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
